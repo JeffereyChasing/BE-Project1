2831ef2867c5fbc8267bb3fe50e2ea09c3f50a83
YanqinWu <wu1149823510@gmail.com>
2022-09-05 13:06:25 +0800

    404

--- index.js
+++ index.js
@@ -15,10 +15,9 @@ app.get('/',(req,res) => {
   //res.send("Hello World!")
 })
 
-app.get("*",(req,res) => {
-    res.status(404).send("<h1> 404 Page </h1>")
-  
-  })
+app.get('*', (req, res) => {
+  res.render('error404')
+})
 
 app.listen(process.env.PORT)
 
